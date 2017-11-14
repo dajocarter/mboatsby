652ed1ed12c59a04bc4b24494329f1ada257d3b9
David Carter <dajocarter@gmail.com>
2017-11-14 00:13:01 -0500

    Add gatsby-source-wordpress to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,18 @@ module.exports = {
   siteMetadata: {
     title: `My Brain on Anatomy`
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `mboa.tribestaging.com`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        useACF: true,
+        verboseOutput: true
+      }
+    }
+  ]
 };
