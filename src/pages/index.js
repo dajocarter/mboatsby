80e98bc298d48fdf335eab64aaee015417eb5a7c
David Carter <dajocarter@gmail.com>
2018-02-27 01:32:46 -0500

    Include link to working case on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to={`/a-curious-cardiovascular-case`}>
+      A Curious Cardiovascular Case
+    </Link>
   </div>
 );
 
