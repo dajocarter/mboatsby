ff00370fef7778274c7ae1d6868b09ff668c561f
David Carter <dajocarter@gmail.com>
2018-02-27 00:01:35 -0500

    Add links to Links component

--- src/components/layouts/Links.js
+++ src/components/layouts/Links.js
@@ -1,14 +1,22 @@
 import React from "react";
 import PropTypes, { string } from "prop-types";
+import Link from "gatsby-link";
 
 const Links = ({ acf }) => {
-  return <div dangerouslySetInnerHTML={{ __html: acf.context }} />;
+  return (
+    <div>
+      <div className="wp-content" dangerouslySetInnerHTML={{ __html: acf.context }} />
+      {acf.links.map((button, index) => (
+        <Link className={`btn btn-primary btn-block`} key={index} to={`/${button.page.post_name}`}>{button.text}</Link>
+      ))}
+    </div>
+  );
 };
 
 export default Links;
 
 Links.propTypes = {
   acf: PropTypes.shape({
-    context: PropTypes.string.isRequired
+    context: PropTypes.string
   })
 };
