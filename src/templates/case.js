b16330eb74bcb73e18ee213ff9351547c3fbbd68
David Carter <dajocarter@gmail.com>
2018-03-01 02:11:27 -0500

    Style headers in Case template

--- src/templates/case.js
+++ src/templates/case.js
@@ -6,15 +6,27 @@ import Content from "../components/layouts/Content";
 import Image from "../components/layouts/Image";
 import Links from "../components/layouts/Links";
 import { Grid } from "react-bootstrap";
+import styled from "styled-components";
+
+const SiteTitle = styled.h1`
+  border-bottom: 1px solid #333;
+  padding-bottom: 0.5rem;
+  letter-spacing: 1px;
+`;
+
+const CaseTitle = styled.h2`
+  margin: 1rem 0 2rem;
+  text-align: center;
+`;
 
 const CaseTemplate = props => (
   <Grid>
     {props.data.tags.edges
       .filter(({ node }) => node.wordpress_id === props.data.case.tags[0])
-      .map(({ node }) => <h1>{node.name}</h1>)}
+      .map(({ node }) => <SiteTitle>{node.name}</SiteTitle>)}
     {props.data.categories.edges
       .filter(({ node }) => node.wordpress_id === props.data.case.categories[0])
-      .map(({ node }) => <h2>{node.name}</h2>)}
+      .map(({ node }) => <CaseTitle>{node.name}</CaseTitle>)}
     {props.data.case.acf.layouts_case &&
       props.data.case.acf.layouts_case.map((acf_type, index) => {
         switch (acf_type.__typename) {
