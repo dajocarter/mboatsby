e0061ca395d948bf4194e83a126c58db5588ade3
David Carter <dajocarter@gmail.com>
2018-02-28 21:39:13 -0500

    Create footer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -38,6 +38,39 @@ const Header = props => (
   </nav>
 );
 
+const Footer = props => (
+  <footer>
+    <div className="container">
+      <h2 className="navbar-brand">
+        <Link to={`/`}>{props.siteMetadata.title}</Link>
+      </h2>
+      <div className="row">
+        <div className="col-xs-12 col-sm-4">
+          <ul>
+            <li>{props.siteMetadata.author.name}</li>
+            <li>
+              <Link to={`mailto:${props.siteMetadata.author.email}`}>
+                {props.siteMetadata.author.email}
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-xs-12 col-sm-6 col-sm-offset-2">
+          <p>{props.siteMetadata.description}</p>
+        </div>
+        <div className="col-xs-12">
+          <a href={`//www.netlify.com`} target={`_blank`}>
+            <img
+              src="https://www.netlify.com/img/global/badges/netlify-light.svg"
+              alt={`Deploys by Netlify`}
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+
 const TemplateWrapper = ({ data, children }) => (
   <div>
     <Helmet
@@ -52,6 +85,7 @@ const TemplateWrapper = ({ data, children }) => (
       menu={data.wordpressWpApiMenusMenusItems.items}
     />
     <main className="container">{children()}</main>
+    <Footer siteMetadata={data.site.siteMetadata} />
   </div>
 );
 
@@ -67,6 +101,11 @@ export const indexQuery = graphql`
     site {
       siteMetadata {
         title
+        description
+        author {
+          name
+          email
+        }
       }
     }
     wordpressWpApiMenusMenusItems(wordpress_id: { eq: 5 }) {
