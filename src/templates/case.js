dd165f4dd5cb59cd390a6ef3dbfeb3f735bc6430
David Carter <dajocarter@gmail.com>
2018-03-01 01:48:07 -0500

    Convert Case template to stateless component

--- src/templates/case.js
+++ src/templates/case.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import CheckYourAnswer from "../components/layouts/CheckYourAnswer";
 import Checkboxes from "../components/layouts/Checkboxes";
@@ -7,73 +7,67 @@ import Image from "../components/layouts/Image";
 import Links from "../components/layouts/Links";
 import { Grid } from "react-bootstrap";
 
-class CaseTemplate extends Component {
-  render() {
-    const page = this.props.data.wordpressWpCases;
-
-    return (
-      <Grid>
-        <h1 dangerouslySetInnerHTML={{ __html: page.title }} />
-        {page.acf.layouts_case &&
-          page.acf.layouts_case.map((acf_type, index) => {
-            switch (acf_type.__typename) {
-              case "WordPressAcf_check_your_answer":
-                return (
-                  <CheckYourAnswer
-                    key={`layout-${index}-${acf_type.__typename}`}
-                    layoutIndex={index}
-                    acf={acf_type}
-                  />
-                );
-                break;
-              case "WordPressAcf_checkboxes":
-                return (
-                  <Checkboxes
-                    key={`layout-${index}-${acf_type.__typename}`}
-                    layoutIndex={index}
-                    acf={acf_type}
-                  />
-                );
-                break;
-              case "WordPressAcf_content":
-                return (
-                  <Content
-                    key={`layout-${index}-${acf_type.__typename}`}
-                    layoutIndex={index}
-                    acf={acf_type}
-                  />
-                );
-                break;
-              case "WordPressAcf_image":
-                return (
-                  <Image
-                    key={`layout-${index}-${acf_type.__typename}`}
-                    layoutIndex={index}
-                    acf={acf_type}
-                  />
-                );
-                break;
-              case "WordPressAcf_links":
-                return (
-                  <Links
-                    key={`layout-${index}-${acf_type.__typename}`}
-                    layoutIndex={index}
-                    acf={acf_type}
-                  />
-                );
-                break;
-            }
-          })}
-      </Grid>
-    );
-  }
-}
+const CaseTemplate = props => (
+  <Grid>
+    <h1>{props.data.case.title}</h1>
+    {props.data.case.acf.layouts_case &&
+      props.data.case.acf.layouts_case.map((acf_type, index) => {
+        switch (acf_type.__typename) {
+          case "WordPressAcf_check_your_answer":
+            return (
+              <CheckYourAnswer
+                key={`layout-${index}-${acf_type.__typename}`}
+                layoutIndex={index}
+                acf={acf_type}
+              />
+            );
+            break;
+          case "WordPressAcf_checkboxes":
+            return (
+              <Checkboxes
+                key={`layout-${index}-${acf_type.__typename}`}
+                layoutIndex={index}
+                acf={acf_type}
+              />
+            );
+            break;
+          case "WordPressAcf_content":
+            return (
+              <Content
+                key={`layout-${index}-${acf_type.__typename}`}
+                layoutIndex={index}
+                acf={acf_type}
+              />
+            );
+            break;
+          case "WordPressAcf_image":
+            return (
+              <Image
+                key={`layout-${index}-${acf_type.__typename}`}
+                layoutIndex={index}
+                acf={acf_type}
+              />
+            );
+            break;
+          case "WordPressAcf_links":
+            return (
+              <Links
+                key={`layout-${index}-${acf_type.__typename}`}
+                layoutIndex={index}
+                acf={acf_type}
+              />
+            );
+            break;
+        }
+      })}
+  </Grid>
+);
 
 export default CaseTemplate;
 
 export const caseQuery = graphql`
   query currentCaseQuery($slug: String!) {
-    wordpressWpCases(slug: { eq: $slug }) {
+    case: wordpressWpCases(slug: { eq: $slug }) {
       title
       acf {
         layouts_case {
