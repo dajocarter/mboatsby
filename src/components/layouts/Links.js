030007434374927ace7c1ddd3c0aedc031b58037
David Carter <dajocarter@gmail.com>
2018-03-01 15:51:52 -0500

    Wrap button links in Cols

--- src/components/layouts/Links.js
+++ src/components/layouts/Links.js
@@ -1,20 +1,22 @@
 import React from "react";
 import PropTypes, { number } from "prop-types";
 import Link from "gatsby-link";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { columnClasses } from "../../utils/helpers";
 
 const Links = ({ acf, layoutIndex }) => {
   return (
     <Row id={`layout-${layoutIndex}`}>
       {acf.links &&
         acf.links.map((button, index) => (
-          <Link
-            className={`btn btn-primary btn-block`}
-            key={index}
-            to={`/${button.page.post_name}`}
-          >
-            {button.text}
-          </Link>
+          <Col key={index} sm={columnClasses(index, acf.links.length)}>
+            <Link
+              className={`btn btn-primary btn-block`}
+              to={`/${button.page.post_name}`}
+            >
+              {button.text}
+            </Link>
+          </Col>
         ))}
     </Row>
   );
