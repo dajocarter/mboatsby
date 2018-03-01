b2cd26b91abb001a3e500465a0e00f7877a0cb00
David Carter <dajocarter@gmail.com>
2018-03-01 15:51:35 -0500

    Add Grid to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "gatsby-link";
+import { Grid } from "react-bootstrap";
 
 const IndexPage = () => (
-  <div>
+  <Grid>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to={`/accc-intro/`}>A Curious Cardiovascular Case</Link>
-  </div>
+  </Grid>
 );
 
 export default IndexPage;
