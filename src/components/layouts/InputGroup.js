d7ebad780b158d5372467c100018896e3f25189d
David Carter <dajocarter@gmail.com>
2018-03-01 23:02:31 -0500

    Transition InputGroup to stateless component

--- src/components/layouts/InputGroup.js
+++ src/components/layouts/InputGroup.js
@@ -1,44 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Row, Col, Checkbox, Radio } from "react-bootstrap";
 import InputCheckbox from "../InputCheckbox";
 import InputRadio from "../InputRadio";
 
-export default class InputGroup extends Component {
-  renderInputs() {
-    switch (this.props.acf.input_type) {
-      case "checkbox":
-        return this.renderCheckboxes();
-        break;
-      case "radio":
-        return this.renderRadios();
-        break;
-    }
-  }
-  renderCheckboxes = () =>
-    this.props.acf.options.map((option, index) => (
-      <InputCheckbox
-        key={index}
-        isCorrect={option.is_correct}
-        label={option.text}
-      />
-    ));
-  renderRadios = () =>
-    this.props.acf.options.map((option, index) => (
-      <InputRadio
-        key={index}
-        isCorrect={option.is_correct}
-        label={option.text}
-        name={`layout-${this.props.layoutIndex}`}
-      />
-    ));
-  render() {
-    return (
-      <Row id={`layout-${this.props.layoutIndex}`}>
-        <Col xs={12}>{this.renderInputs()}</Col>
-      </Row>
-    );
-  }
-}
+const InputGroup = props => (
+  <Row id={`layout-${props.layoutIndex}`}>
+    <Col xs={12}>
+      {(() => {
+        switch (props.acf.input_type) {
+          case "checkbox":
+            return (() =>
+              props.acf.options.map((option, index) => (
+                <InputCheckbox
+                  key={index}
+                  isCorrect={option.is_correct}
+                  label={option.text}
+                />
+              )))();
+            break;
+          case "radio":
+            return (() =>
+              props.acf.options.map((option, index) => (
+                <InputRadio
+                  key={index}
+                  isCorrect={option.is_correct}
+                  label={option.text}
+                  name={`layout-${props.layoutIndex}`}
+                />
+              )))();
+            break;
+        }
+      })()}
+    </Col>
+  </Row>
+);
+
+export default InputGroup;
 
 InputGroup.propTypes = {};
