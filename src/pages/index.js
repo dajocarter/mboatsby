106286f0854b8e5429e03ee3a1a2991fb22705e8
David Carter <dajocarter@gmail.com>
2018-03-09 21:02:49 -0500

    Update index page to use acf fields

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,117 @@
 import React from "react";
 import Link from "gatsby-link";
-import { Grid } from "react-bootstrap";
+import Img from "gatsby-image";
+import { Grid, Row, Col } from "react-bootstrap";
+import styled from "styled-components";
+import { columnClasses } from "../utils/helpers";
 
-const IndexPage = () => (
+const CircleImg = styled(Img)`
+  height: 250px;
+  width: 250px;
+  border-radius: 50%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const ColumnTitle = styled.h3`
+  text-align: center;
+`;
+
+const ButtonLink = styled(Link)`
+  max-width: 250px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const IndexPage = props => (
   <Grid>
-    <h1>Try out these cases</h1>
-    <p>
-      <Link to={`/a-curious-cardiovascular-case/accc-intro/`}>
-        A Curious Cardiovascular Case
-      </Link>
-    </p>
-    <p>
-      <Link to={`/a-mysterious-mass/amm-intro/`}>A Mysterious Mass</Link>
-    </p>
-    <p>
-      <Link to={`/the-suspicious-lesion/tsl-intro/`}>
-        The Suspicious Lesion
-      </Link>
-    </p>
-    <p>
-      <Link to={`/a-night-in-the-er/ner-intro/`}>A Night in the E.R.</Link>
-    </p>
+    <h1>{props.data.page.title}</h1>
+    {props.data.page.acf.rows.map((row, rowIndex) => (
+      <Row key={rowIndex}>
+        <Col xs={12}>
+          <h2>{row.row_title}</h2>
+        </Col>
+        {row.columns.map((column, columnIndex) => {
+          const colCase = props.data.cases.edges.filter(
+            ({ node }) => node.wordpress_id === column.column_link.wordpress_id
+          )[0].node;
+          const category = props.data.categories.edges.filter(
+            ({ node }) => node.wordpress_id === colCase.categories[0]
+          )[0].node;
+          return (
+            <Col
+              key={columnIndex}
+              xs={12}
+              sm={columnClasses(columnIndex, row.columns.length)}
+            >
+              <CircleImg
+                sizes={column.column_image.localFile.childImageSharp.sizes}
+              />
+              <ColumnTitle>
+                <Link to={`/${category.slug}/${column.column_link.post_name}`}>
+                  {column.column_title}
+                </Link>
+              </ColumnTitle>
+              <div dangerouslySetInnerHTML={{ __html: column.column_text }} />
+              <ButtonLink
+                className={`btn btn-primary btn-block`}
+                to={`/${category.slug}/${column.column_link.post_name}`}
+              >
+                {column.button_text}
+              </ButtonLink>
+            </Col>
+          );
+        })}
+      </Row>
+    ))}
   </Grid>
 );
 
 export default IndexPage;
+
+export const homeQuery = graphql`
+  query homeQuery {
+    page: wordpressPage(wordpress_id: { eq: 7 }) {
+      title
+      acf {
+        rows {
+          row_title
+          columns {
+            column_image {
+              localFile {
+                childImageSharp {
+                  sizes(maxWidth: 250) {
+                    ...GatsbyImageSharpSizes_tracedSVG
+                  }
+                }
+              }
+            }
+            column_title
+            column_link {
+              wordpress_id
+              post_name
+            }
+            column_text
+            button_text
+          }
+        }
+      }
+    }
+    cases: allWordpressWpCases {
+      edges {
+        node {
+          wordpress_id
+          categories
+        }
+      }
+    }
+    categories: allWordpressCategory {
+      edges {
+        node {
+          wordpress_id
+          slug
+        }
+      }
+    }
+  }
+`;
