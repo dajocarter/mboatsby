c66076932f093dc22a62e894ce7266c976cc262b
David Carter <dajocarter@gmail.com>
2018-03-10 18:36:47 -0500

    Update signOut button to bootstrap

--- src/components/firebase/SignOut.js
+++ src/components/firebase/SignOut.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { auth } from "../../firebase";
+import { Button } from "react-bootstrap";
 
-const SignOutButton = () => (
-  <button type="button" onClick={auth.doSignOut}>
-    Sign Out
-  </button>
-);
+const SignOutButton = () => <Button onClick={auth.doSignOut}>Sign Out</Button>;
 
 export default SignOutButton;
