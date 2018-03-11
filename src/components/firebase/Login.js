2bd1f7bf5af70152e354efa13752ff26c375efca
David Carter <dajocarter@gmail.com>
2018-03-10 19:47:47 -0500

    Update login form to bootstrap

--- src/components/firebase/Login.js
+++ src/components/firebase/Login.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Link from "gatsby-link";
 import { auth } from "../../firebase";
+import { FormGroup, FormControl, Button } from "react-bootstrap";
+import styled from "styled-components";
+
+const Form = styled.form`
+  background: #e0e0e0;
+  margin: 6rem 0;
+  padding: 2rem;
+`;
+
+const FormTitle = styled.h1`
+  margin-top: 0;
+  text-align: center;
+`;
+
+const FormLinks = styled(FormControl.Static)`
+  text-align: center;
+  && {
+    padding: 1.5rem 0 0;
+  }
+`;
 
 const updateByPropertyName = (propertyName, value) => () => ({
   [propertyName]: value
@@ -12,7 +33,7 @@ const INITIAL_STATE = {
   error: null
 };
 
-class SignInForm extends Component {
+class LoginForm extends Component {
   constructor(props) {
     super(props);
 
@@ -43,31 +64,43 @@ class SignInForm extends Component {
     const isInvalid = password === "" || email === "";
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          value={email}
-          onChange={event =>
-            this.setState(updateByPropertyName("email", event.target.value))
-          }
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          value={password}
-          onChange={event =>
-            this.setState(updateByPropertyName("password", event.target.value))
-          }
-          type="password"
-          placeholder="Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
+      <Form onSubmit={this.onSubmit}>
+        <FormTitle>My Brain on Anatomy</FormTitle>
+        <FormGroup controlId="email">
+          <FormControl
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={event =>
+              this.setState(updateByPropertyName("email", event.target.value))
+            }
+          />
+        </FormGroup>
+        <FormGroup controlId="password">
+          <FormControl
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={event =>
+              this.setState(
+                updateByPropertyName("password", event.target.value)
+              )
+            }
+          />
+        </FormGroup>
+        <Button block bsStyle="primary" disabled={isInvalid} type="submit">
+          Login
+        </Button>
 
-        {error && <p>{error.message}</p>}
-      </form>
+        {error && <FormControl.Static>{error.message}</FormControl.Static>}
+
+        <FormLinks>
+          <Link to="/signup/">Create an account</Link> or{" "}
+          <Link to="reset-password">reset password</Link>
+        </FormLinks>
+      </Form>
     );
   }
 }
 
-export default withRouter(SignInForm);
+export default withRouter(LoginForm);
