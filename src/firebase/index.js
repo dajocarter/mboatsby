8afa82cdfdab8be1eee1e6045f3019c1ef69daf5
David Carter <dajocarter@gmail.com>
2018-05-05 13:15:34 -0400

    Setup firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,29 @@
+import * as firebase from "firebase";
+
+const prodConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+const devConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const storage = firebase.storage();
+
+export { storage };
