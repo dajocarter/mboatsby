fcc00665a37c19f884ac30c45dcc20dc2b6c4e7d
David Carter <dajocarter@gmail.com>
2018-05-08 19:57:58 -0400

    Prefix env vars with GATSBY_

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 import "firebase/storage";
 
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+  apiKey: process.env.GATSBY_API_KEY,
+  authDomain: process.env.GATSBY_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_DATABASE_URL,
+  projectId: process.env.GATSBY_PROJECT_ID,
+  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID
 };
 
 if (!firebase.apps.length) {
