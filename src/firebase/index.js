2fb079420f31c8a9daf80c7569340e360c7e70da
David Carter <dajocarter@gmail.com>
2018-05-08 15:23:01 -0400

    Condense down to one firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,6 @@
 import * as firebase from "firebase";
 
-const prodConfig = {
+const config = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DATABASE_URL,
@@ -8,17 +8,6 @@ const prodConfig = {
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
-
-const devConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
-};
-
-const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
