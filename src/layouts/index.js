02bc3f7096b0da9673abe16ffc33b426be0accc6
David Carter <dajocarter@gmail.com>
2018-05-30 02:18:09 -0400

    Wrap template in Auth to pass down authorization

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,28 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
+import Auth from "../containers/Auth";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
 import "./index.scss";
 
-const TemplateWrapper = ({ data, children }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
-      ]}
-    />
-    <Header
-      title={data.site.siteMetadata.title}
-      menu={data.wordpressWpApiMenusMenusItems.items}
-    />
-    <main>{children()}</main>
-    <Footer siteMetadata={data.site.siteMetadata} />
-  </div>
+const TemplateWrapper = ({ data, children, ...props }) => (
+  <Auth>
+    {auth => {
+      return (
+        <div>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: "description", content: "Sample" },
+              { name: "keywords", content: "sample, something" }
+            ]}
+          />
+          <Header
+            title={data.site.siteMetadata.title}
+            menu={data.wordpressWpApiMenusMenusItems.items}
+            {...auth}
+          />
+          <main>{children({ ...props, auth })}</main>
+          <Footer siteMetadata={data.site.siteMetadata} />
+        </div>
+      );
+    }}
+  </Auth>
 );
 
 TemplateWrapper.propTypes = {
