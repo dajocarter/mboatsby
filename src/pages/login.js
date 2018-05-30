826edfbbb32209c759da56878303fe08ca46fbc2
David Carter <dajocarter@gmail.com>
2018-05-30 14:30:34 -0400

    Create a login page

---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import LoginForm from "../components/forms/Login";
+import { Grid, Row, Col } from "react-bootstrap";
+
+const LoginPage = ({ history, signIn, uid }) => {
+  return (
+    <Grid>
+      <Row>
+        <Col xs={12} sm={6} smOffset={3}>
+          <LoginForm history={history} signIn={signIn} />
+        </Col>
+      </Row>
+    </Grid>
+  );
+};
+
+export default LoginPage;
