8ba7595f0ded0bd475be5b1660b8d3357b33a27b
David Carter <dajocarter@gmail.com>
2018-06-05 00:15:31 -0400

    Convert TemplateWrapper to a lifecycle component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
@@ -9,37 +9,40 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
 import "./index.scss";
 
-const TemplateWrapper = ({ data, children, ...props }) => (
-  <Auth>
-    {auth => {
-      return (
-        <div>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: "description", content: "Sample" },
-              { name: "keywords", content: "sample, something" }
-            ]}
-          />
-          <Header
-            title={data.site.siteMetadata.title}
-            menu={data.wordpressWpApiMenusMenusItems.items}
-            {...auth}
-          />
-          <main>{children({ ...props, ...auth })}</main>
-          <Footer siteMetadata={data.site.siteMetadata} />
-        </div>
-      );
-    }}
-  </Auth>
-);
+export default class TemplateWrapper extends Component {
+  static propTypes = {
+    children: PropTypes.func,
+    data: PropTypes.object.isRequired
+  };
 
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-  data: PropTypes.object.isRequired
-};
-
-export default TemplateWrapper;
+  render() {
+    const { data, children } = this.props;
+    return (
+      <Auth>
+        {auth => {
+          return (
+            <div>
+              <Helmet
+                title={data.site.siteMetadata.title}
+                meta={[
+                  { name: "description", content: "Sample" },
+                  { name: "keywords", content: "sample, something" }
+                ]}
+              />
+              <Header
+                title={data.site.siteMetadata.title}
+                menu={data.menu.items}
+                {...auth}
+              />
+              <main>{children({ ...this.props, ...auth })}</main>
+              <Footer siteMetadata={data.site.siteMetadata} />
+            </div>
+          );
+        }}
+      </Auth>
+    );
+  }
+}
 
 export const indexQuery = graphql`
   query indexQuery {
@@ -53,7 +56,7 @@ export const indexQuery = graphql`
         }
       }
     }
-    wordpressWpApiMenusMenusItems(wordpress_id: { eq: 5 }) {
+    menu: wordpressWpApiMenusMenusItems(wordpress_id: { eq: 5 }) {
       items {
         wordpress_id
         title
